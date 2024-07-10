6271a9d5e892230f00d6f8fdae26252e21ef5234
gyuhaaa <vkvkvk2892@naver.com>
2024-07-10 23:17:39 +0900

    Q28

--- solidity100/solidity30.sol
+++ solidity100/solidity30.sol
@@ -130,8 +130,8 @@ contract Q28 {
     /* 
         ID와 PW를 넣으면 해시함수(keccak256)에 둘을 같이 해시값을 반환해주는 함수를 구현하세요.
     */
-    function hash(string memory _id, string memory _pw) public pure returns(bytes32, bytes32) {
-        return (keccak256(abi.encodePacked(_id)), keccak256(abi.encodePacked(_pw)));
+    function hash(string memory _id, string memory _pw) public pure returns(bytes32) {
+        return (keccak256(abi.encodePacked(_id, _pw)));
     }
 }
 
