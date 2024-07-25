42fc2d484ec421f3b159e30bac4fe7d16504469b
gyuhaaa <vkvkvk2892@naver.com>
2024-07-26 00:34:26 +0900

    solidity60

--- solidity100/solidity60.sol
+++ solidity100/solidity60.sol
@@ -232,10 +232,28 @@ contract Q59 {
 
 contract Q60 {
     /*
-        1. 방이 2개 밖에 없는 펜션을 여러분이 운영합니다. 각 방마다 한번에 3명 이상 투숙객이 있을 수는 없습니다. 특정 날짜에 특정 방에 누가 투숙했는지 알려주는 자료구조와 그 자료구조로부터 값을 얻어오는 함수를 구현하세요.
+        방이 2개 밖에 없는 펜션을 여러분이 운영합니다. 각 방마다 한번에 3명 이상 투숙객이 있을 수는 없습니다. 
+        특정 날짜에 특정 방에 누가 투숙했는지 알려주는 자료구조와 그 자료구조로부터 값을 얻어오는 함수를 구현하세요.
     
         예약시스템은 운영하지 않아도 됩니다. 과거의 일만 기록한다고 생각하세요.
         
         힌트 : 날짜는 그냥 숫자로 기입하세요. 예) 2023년 5월 27일 → 230527
     */
+
+    enum Room { A, B }
+    string[] guests;
+    mapping(uint => mapping(Room => string[])) reservations;
+
+    function addReservation(uint _date, Room _room, string[] memory _guests) public {
+        require(_guests.length < 3, "Too many guests");
+        reservations[_date][_room] = _guests;
+
+        for (uint8 i=0; i < _guests.length; i++) {
+            guests.push(_guests[i]);
+        }
+    }
+
+    function getGuest(uint _date, Room _room) public view returns(string[] memory) {
+        return reservations[_date][_room];
+    }
 }
